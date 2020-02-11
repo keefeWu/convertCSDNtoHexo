0ebc761dabb62e843fb6dcd44eefe6dfdfbf8ad3
keefe <443552864@qq.com>
2020-02-11 21:27:14 +0800

    'use_html_image_label'

--- scratch.py
+++ scratch.py
@@ -7,6 +7,7 @@ from data import *
 from imp import reload
 import re
 import os
+from tqdm import tqdm
 reload(sys)
 
 def getArticleList():
@@ -50,72 +51,58 @@ def download_img(img_url, path):
         print("done")
     del r
 
-def deal_with_img(mdTxt, title, root_url):
-    result = mdTxt
-    tempTxt = mdTxt
+def deal_with_img(html_data, title, root_url):
+    result = html_data
+    tempTxt = html_data
     count = 0
     posList = []
     pos = 0
-    while True:
-        pattern=re.compile(r"!\[(.*?)\]\((.*?)\)")
-        fullid = pattern.search(tempTxt)
-        if fullid is None:
-            break
-        fullstr = tempTxt[fullid.start(): fullid.end()]
-        pattern=re.compile(r"\((.*?)\)")
-        urlid = pattern.search(fullstr)
-        if urlid is None:
-            break
-        posList.append([pos + fullid.start() + urlid.start() + 1,
-            pos + fullid.start() + urlid.end() - 1]) #这里+1和-1是去掉前后的小括号
-        pos += fullid.start() + urlid.end()
-        if pos >= len(mdTxt):
-            break
-        tempTxt = mdTxt[pos:]
-
-    # path = os.path.join("CSDN", title)
-    # if not os.path.exists(path):
-        # os.mkdir(path)
-    
+    soup = BeautifulSoup(html_data,'lxml') 
+    allImg = soup.findAll("img")
     path = title
-    for i in range(len(posList)):
-        pos = posList[-i-1]
-        url = mdTxt[pos[0]:pos[1]]
-        if url[:4] != 'http':
-            url = 'http://i.imgur.com' + url
+    for i in range(len(allImg)):
+        url = allImg[i]['src']
         try:
             r = requests.get(url)
         except Exception as e:
             print(e)
             print(url)
-            print(url[:4])
             continue
+        ori_url = url
         id = url.find('?')
         if id > 0 :
             url = url[:id]
-        img_path = os.path.join('G:\\blog\\keefeWu.github.io\\source\\_posts', path, '%d%s'%(len(posList) - i, os.path.splitext(url)[1]))
+        suffix = os.path.splitext(url)[1]
+        if suffix == '':
+            suffix = '.png'
+        img_name = '%d%s'%(i, suffix)
+        img_path = os.path.join('G:\\blog\\keefeWu.github.io\\source\\_posts', path, img_name)
         with open(img_path, 'wb') as f:
             f.write(r.content)
-        result = result[:pos[0]] + '%d%s'%(len(posList) - i, os.path.splitext(url)[1]) + result[pos[1]:]              
+        result = result.replace(ori_url, img_name)
     return result
     
 
 def main():
     title_list, href_list = getArticleList()
     html_data_list = []
-    for i in range(len(href_list)):
+    for i in tqdm(range(len(href_list))):
         html_data = getHtml(href_list[i])
         html_data_list.append(html_data)
-    for i in range(len(href_list)):
+    for i in tqdm(range(len(href_list))):
         html_data = html_data_list[i]
-        mdTxt = html2text.html2text(html_data)
         print(title_list[i])
         cmd = 'G: && \
                 cd G:\\blog\\keefeWu.github.io &&\
                 hexo new %s'%title_list[i]
         print(cmd)
         os.system(cmd)
-        mdTxt = deal_with_img(mdTxt, title_list[i], href_list[i])
+        title_list[i] = title_list[i].replace('.', '-')
+        title_list[i] = title_list[i].replace('.', '-')
+        title_list[i] = title_list[i].replace('(', '-')
+        title_list[i] = title_list[i].replace(')', '')
+        html_data = deal_with_img(html_data, title_list[i], href_list[i])
+        mdTxt = html2text.html2text(html_data)
         file = open('G:/blog/keefeWu.github.io/source/_posts/%s.md'%title_list[i], 'a', encoding="utf-8")
         file.write(mdTxt)
         file.close()
